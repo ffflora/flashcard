0217747d3591fd9add07cdf2efff8b9b328e505b
flora <avril_99@vii.im>
2021-01-20 07:48:24 -0800

    Updates

---
+++ precache-manifest.dfd372b4ed071e62e360f069c77ff181.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e15fd1e0c05c975c7d7b513ea69240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f95ff3f9de46cea0fdf9",
+    "url": "/static/css/main.46edf5b7.chunk.css"
+  },
+  {
+    "revision": "5a569fd1e75040dc91c8",
+    "url": "/static/js/2.fd433f0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd433f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f95ff3f9de46cea0fdf9",
+    "url": "/static/js/main.7c2c1e65.chunk.js"
+  },
+  {
+    "revision": "503f93fa3a22b8ad9b7a",
+    "url": "/static/js/runtime-main.aadc0848.js"
+  }
+]);
